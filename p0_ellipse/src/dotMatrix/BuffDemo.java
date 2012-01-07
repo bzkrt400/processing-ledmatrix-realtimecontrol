decfd6648999a3e24364451a5b232d5fea40461d
weihong.guan <Weihong.Guan@gmail.com>
2012-01-07 16:26:01 +0000

    FFT Power Spectrum Initialization

--- p0_ellipse/src/dotMatrix/BuffDemo.java
+++ p0_ellipse/src/dotMatrix/BuffDemo.java
@@ -1,26 +1,23 @@
 package dotMatrix;
 
 import processing.core.PApplet;
-import processing.core.PFont;
-
 import ddf.minim.*;
-import ddf.minim.analysis.*;
 
 public class BuffDemo extends PApplet
 {
 	private static final long serialVersionUID = 8598839469711789068L;
 	Minim minim;
 	AudioPlayer ap;
-	FFT fft;
+
 	float r =0f;
-	PFont font;
 	
 	int halfWidth, halfHeight;
 	float sectionWidth;
 	
+	float buffMax, buffMin;
+	
 	public void setup()
 	{
-		font = this.loadFont("Calibri-24.vlw");
 		size(600, 600, P2D);	
 		
 		halfWidth = width / 2;
@@ -30,71 +27,45 @@ public class BuffDemo extends PApplet
 		background(0x00);
   
 		ap = minim.loadFile("sample.mp3", 512);
+		ap.loop();		
 		
-		sectionWidth = (float)width / ap.bufferSize();
-
-		ap.loop();
-		
-
-		
-		  textFont(font);
-		  fill(0);
-		  text(ap.bufferSize(), 0, 24);
+		sectionWidth = (float)width / ap.bufferSize();		
 	}
 	
 	public void draw()
 	{
+		TestHelper.PrintText(this, Float.toString(buffMax));
+		TestHelper.PrintText(this, Float.toString(buffMin), 0xff, 2f, 48f);
+		
 		r+=0.02;
-		fade(20);
-		 //background(0);
-		  //fill(255);
-		  /*
-		  fft.forward(ap.mix);
-		  int w = (int)(width/fft.avgSize());
-		  for(int i = 0; i < fft.avgSize(); i++)
-		  {
-			  // draw a rectangle for each average, multiply the value by 5 so we can see it better
-			  rect(i*w, height, i*w + w, height - fft.getAvg(i));
-		  }
-		  */
-		  //background(0);
+		fade(20);		 
 			
-		  stroke(0xffffffff);
-		  strokeWeight(2);  
-		  // we draw the waveform by connecting neighbor values with a line
-		  // we multiply each of the values by 50 
-		  // because the values in the buffers are normalized
-		  // this means that they have values between -1 and 1. 
-		  // If we don't scale them up our waveform 
-		  // will look more or less like a straight line.
-		  
-		  pushMatrix();
-			
-		translate(halfWidth, halfHeight);
-			rotate(r);
-			scale(1.4f);
-					    
-		  
-		  for(int i = 1; i < ap.bufferSize(); i++)
-		  {
-		    line((i-1)*sectionWidth - halfWidth, ap.left.get(i-1)*250, i*sectionWidth - halfWidth, ap.left.get(i)*250);
-		    		    
-		  }
-		  popMatrix();
-  
-		  pushMatrix();
-			
-			translate(halfWidth, halfHeight);
-				rotate(r+PI/2);
-				scale(1.4f);		    
-			  
-			  for(int i = 1; i < ap.bufferSize(); i++)
-			  {
-			   
-			    line((i-1)*sectionWidth - halfWidth,ap.left.get(i-1)*250, i*sectionWidth - halfWidth, ap.left.get(i)*250);		    
-			  }
-			  popMatrix();
+		stroke(0xffffffff);
+		strokeWeight(2);  
+		 
+		drawWave(ap.left, r);  
+		drawWave(ap.right, r+PI/2);	
+		
   		delay(50);
+	}
+	
+	private void drawWave(AudioBuffer ab, float angle)
+	{
+		pushMatrix();
+		
+		translate(halfWidth, halfHeight);
+		rotate(angle);
+		scale(1.4f);					    
+		  
+		for(int i = 1; i < ab.size(); i++)
+		{
+			if (ab.get(i)>buffMax) buffMax = ab.get(i);
+			if (ab.get(i)<buffMin) buffMin = ab.get(i);
+			
+			line((i-1)*sectionWidth - halfWidth, ab.get(i-1)*100, i*sectionWidth - halfWidth, ab.get(i)*100);			
+		}
+		  
+		popMatrix();
 	}
 	
 	private void fade(int fadeValue)
@@ -106,9 +77,8 @@ public class BuffDemo extends PApplet
 	public void stop()
 	{
 	  // always close Minim audio classes when you finish with them
-	  ap.close();
-	  minim.stop();
-	  
-	  super.stop();
+		ap.close();
+		minim.stop();
+		super.stop();
 	}
 }
