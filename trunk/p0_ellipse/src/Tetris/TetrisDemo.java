a27bc2cd6707074785d238b8f7b905c1cf64ed0e
Weihong.Guan@gmail.com <Weihong.Guan@gmail.com@b43b6bb4-d061-4591-6739-4d82097d910d>
2012-01-10 17:23:26 +0000

    Tetris KeyBoard control

--- trunk/p0_ellipse/src/Tetris/TetrisDemo.java
+++ trunk/p0_ellipse/src/Tetris/TetrisDemo.java
@@ -24,9 +24,42 @@ public class TetrisDemo extends PApplet
 	}
 	
 	public void keyPressed()
-	{
+	{		
+		TetrisDirection t = null;
+		
+		switch(key) 
+		{	
+			case CODED:					
+				switch(keyCode)
+				{
+					case DOWN:
+						t = TetrisDirection.DOWN;
+						break;					
+					case UP:
+						t = TetrisDirection.UP;
+						break;
+					case LEFT:
+						t = TetrisDirection.LEFT;
+						break;
+					case RIGHT:
+						t = TetrisDirection.RIGHT;
+						break;
+					default:
+						break;
+				}
+				break;
+			case ' ':
+				t = TetrisDirection.CLOCKWISE;
+				break;
+			default:
+				break;
+		}
+		
+		if (t != null)
+			tb.change(t);
+		
 		dm.clear(false);
-		tb.turn();
+		//tb.turn();
 		tb.show();
 		dmd.display();
 	}
